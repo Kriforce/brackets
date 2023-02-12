c42a690d2aa1dc9c4f4e8e3a6e5abd9bc2b74056
Kristaps <kriforce7@gmail.com>
2023-02-12 14:14:13 +0300

    create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let start = [];
+  let end = [];
+  let chars = str.split('');
+  let res = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    start.push(bracketsConfig[i][0]);
+    end.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < chars.length; i++) {
+    let charStr = chars[i];
+
+    if (start.indexOf(charStr) > -1) {
+      if (res[res.length - 1] == charStr && end.indexOf(charStr) > -1) {
+        res.pop();
+      } else {
+        res.push(charStr);
+      }
+    } else {
+      let endStr = res.pop();
+      let indexEnd = end.indexOf(charStr);
+
+      if (indexEnd < 0) {
+        return false;
+      }
+
+      if (start[indexEnd] != endStr) {
+        return false
+      }
+    }
+  }
+
+  return res.length == 0;
 }
